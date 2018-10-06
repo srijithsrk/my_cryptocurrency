34f3e27da26267ca81e7658142ea518ab1ec5919
Srijith <30682653+srijithsrk@users.noreply.github.com>
2018-10-07 03:22:18 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,16 @@
-crypto
+//my token
+pragma solidity ^0.4.0;
+
+contract DragonStone
+{
+address public creator;
+mapping(address => uint) public balance;   //mapping acts as a key value pair like that of an array
+function.DragonStone()
+{
+creator = msg.sender;
+}
+function createCoin(address _reciever, uint amount)
+{
+if(msg.sender != creator ) throw;
+balance[_reciever] += amount;
+}
